878d3684735504d000972f1689f4eca1a82c3041
Gaetan Semet <gaetan@xeberon.net>
2017-06-14 22:35:18 +0200

    just creating
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

---
+++ txwebbackendbase/tests/test_txwebbackendbase.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_txwebbackendbase
+----------------------------------
+Tests for `txwebbackendbase` module.
+"""
+
+import pytest
+
+from txwebbackendbase import txwebbackendbase
+
+
+class TestTxwebbackendbase(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
