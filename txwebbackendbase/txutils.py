92d43346dc4c6850a2bb026606898ba4b3653602
Gaetan Semet <gaetan@xeberon.net>
2017-10-15 13:58:43 +0200

    split txutils
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- txwebbackendbase/txutils.py
+++ txwebbackendbase/txutils.py
@@ -23,153 +23,7 @@ import sys
 import time
 
 from twisted.internet import defer
-from twisted.internet import threads
 from twisted.python.failure import Failure
-
-
-def deferredAsThread(func):
-    """This decorator wraps functions and returns deferred
-       See twisted.internet.threads.deferToThread
-       you should only use this decorator if there are no
-       side effects .. ie you are only emitting data, not
-       modifying class or server state inside of the thread.
-       @return (defer.Deferred)
-    """
-
-    def newfunc(*args, **kwargs):
-        return threads.deferToThread(func, *args, **kwargs)
-
-    return newfunc
-
-
-def deferredInThreadPool(pool=None, R=None):
-    """This decorator will place a method into a threadpool.
-       If you don't provide a pool, a default will be guessed.
-       See twisted.internet.threads.deferToThreadPool
-       you should only use this decorator if there are no
-       side effects .. ie you are only emitting data, not
-       modifying class or server state inside of the thread.
-       @param R (reactor)
-       @return (defer.Deferred)
-    """
-    if not R:
-        from twisted.internet import reactor
-        R = reactor
-    if not pool:  # provide a default thread pool
-        pool = R.getThreadPool()
-
-    def decorator(func):
-        def newfunc(*a, **kw):
-            """I call blocking python code in a seperate thread.
-               I return a deferred and will fire callbacks or errbacks.
-            """
-            return threads.deferToThreadPool(R, pool, func, *a, **kw)
-
-        return newfunc
-
-    return decorator
-
-
-def synchronizedInThread(R=None):
-    """This decorator will make a blocking call from a thread to
-       the reactor and wait for all callbacks.  the result will
-       be from the callback chain. Effectively useless unless
-       you are in a seperate thread.
-       #only good use case for this is in the decorators below
-       ``threadedSafeDeferred`` and ``threadedSafePoolDeferred``
-        ... use of this decorator is discouraged unless you
-       know what it does.
-       @param R (reactor)
-       @raise any exception
-       @return (object)
-    """
-    if not R:
-        from twisted.internet import reactor
-        R = reactor
-
-    def decorator(func):
-        def newfunc(*a, **kw):
-            # works with deferred and blocking methods
-            return threads.blockingCallFromThread(R, func, *a, **kw)
-
-        return newfunc
-
-    return decorator
-
-
-def threadSafeDeferred(R=None):
-    """This decorator will run a method in a seperate thread and
-       wait for all callbacks/errbacks to fire before firing its
-       own callbacks/errbacks.
-       you should only use this decorator if there ARE
-       side effects .. ie you are modifying class or server state
-       inside of the thread.
-       @param R (reactor)
-       @return (defer.Deferred)
-    """
-    if not R:
-        from twisted.internet import reactor
-        R = reactor
-
-    def decorator(func):
-        def newfunc(*a, **kw):
-            return func(*a, **kw)
-
-        # decorate newfunc in a blocking reactor call
-        return synchronizedInThread(R)(newfunc)
-
-    # decorate decorator in a thread
-    return deferredAsThread(decorator)
-
-
-def threadSafePoolDeferred(pool=None, R=None):
-    """This decorator will run a method in a thread pool and
-       wait for all callbacks/errbacks to fire before firing its
-       own callbacks/errbacks.  If you don't provide a thread
-       pool, then a default will be selected for you.
-       you should only use this decorator if there ARE
-       side effects .. ie you are modifying class or server state
-       inside of the thread.
-       @param pool (thread pool object)
-       @param R (reactor)
-       @return (defer.Deferred)
-    """
-    if not R:
-        from twisted.internet import reactor
-        R = reactor
-    if not pool:  # pick a thread pool
-        pool = R.getThreadPool()
-
-    def decorator(func):
-        def newfunc(*a, **kw):
-            return func(*a, **kw)
-
-        # decorate newfunc in a blocking reactor call
-        return synchronizedInThread(R)(newfunc)
-
-    # decorate decorator in a thread pool
-    return deferredInThreadPool(pool, R)(decorator)
-
-
-def synchronizedDeferred(lock):
-    """The function will run with the given lock acquired"""
-    # make sure we are given an acquirable/releasable object
-    assert isinstance(lock, (defer.DeferredLock, defer.DeferredSemaphore))
-
-    def decorator(func):
-        """Takes a function that will aquire a lock, execute the function,
-           and release the lock.  Control will then be returned to the
-           reactor for err/callbacks to run.
-
-           returns deferred
-        """
-
-        def newfunc(*args, **kwargs):
-            return lock.run(func, *args, **kwargs)
-
-        return newfunc
-
-    return decorator
 
 
 def raises(exc):
@@ -504,8 +358,7 @@ class _InspectorGadget(object):
         """display the calling arguments"""
         argsString = ','.join(map(str, self.args))
         if self.kwargs:
-            argsString += (',' +
-                           ','.join(["%s=%s" % pair for pair in self.kwargs.items()]))
+            argsString += (',' + ','.join(["%s=%s" % pair for pair in self.kwargs.items()]))
         self.write(">>> Calling %s(%s)" % (self.function.func_name, argsString))
 
     def __call__(self, *args, **kwargs):
@@ -561,16 +414,11 @@ class InvalidArgsLengthException(Exception):
 
 
 _EXPORTS = [
-    'deferredAsThread',
-    'deferredInThreadPool',
-    'synchronizedInThread',
-    'threadSafeDeferred',
-    'threadSafePoolDeferred',
-    'synchronizedDeferred',
     'safe',
     'debugCall',
     'retry',
     'typesafety',
+    'InvalidArgsLengthException',
 ]
 
 # export our methods
